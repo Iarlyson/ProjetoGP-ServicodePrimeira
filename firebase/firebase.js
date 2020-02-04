ac529e22848b3829c7564f8d6a628ab94082dc90
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2020-02-04 16:24:58 -0300

    Fenix

---
+++ firebase/firebase.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyASAVwwuvz7FHM6ZajfkqT364PupuMcB74",
+    authDomain: "gp-servicodeprimeira.firebaseapp.com",
+    databaseURL: "https://gp-servicodeprimeira.firebaseio.com",
+    projectId: "gp-servicodeprimeira",
+    storageBucket: "gp-servicodeprimeira.appspot.com",
+    messagingSenderId: "534134505996",
+    appId: "1:534134505996:web:02c270d36533c55344a429",
+    measurementId: "G-9RM2TT917R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+        
---
+++ materialize/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
