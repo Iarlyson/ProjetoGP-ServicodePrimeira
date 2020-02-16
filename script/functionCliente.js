7e77a1cd0d57f85bddab570c97a67085952be5f0
Sidney002 <caiosidney69@gmail.com>
2020-02-16 14:51:37 -0300

    buscar profissional att

--- script/functionCliente.js
+++ script/functionCliente.js
@@ -1,15 +1,17 @@
 function pesquisar(){
     var main = document.getElementById("resultado");
     var Profissao = document.getElementById("Profissao").value;
-    var resultado = "<h1>Profissionais</h1>"
+    var resultado= "<h3 class=\"center-align\">Profissionais</h3>";
     firebase.database().ref('Usuarios').on('value', function (snapshot) {
         snapshot.forEach(function (item) {
                 if(Profissao==item.val().Profissao){
+                    resultado += "<div style=\"border: 1px solid black;padding: 10px; margin-top: 10px; border-radius: 15px;\">";
                     resultado += "<h3>"+item.val().Nome+"</h3>";
-                    resultado += "<p>"+item.val().Profissao+"</p>";
-                    resultado += "<p>"+item.val().Descricao+"</p>";
-                    resultado +="<a href=\"tel:"+item.val().Telefone+"\">"+ item.val().Telefone+"</a>"
-                    resultado += "<p>Avaliação: " +item.val().Media+"</p><br/>";
+                    resultado += "<p><span>"+item.val().Profissao+"</span><br>";
+                    resultado += "<span>"+item.val().Descricao+"</span><br>";
+                    resultado +="<a href=\"tel:"+item.val().Telefone+"\">"+ item.val().Telefone+"</a><br>"
+                    resultado += "<span>Avaliação: " +item.val().Media+"</span></p>";
+                    resultado += "</div>"
                 }
                 if(Profissao=="Todos" && item.val().Profissional=="true"){
                     resultado += "<h3>"+item.val().Nome+"</h3>";
